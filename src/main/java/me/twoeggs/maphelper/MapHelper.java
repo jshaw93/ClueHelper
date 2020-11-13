c81babf30704a9599b46fb2181f5360a860c00bb
Tyrrel <jshaw93@gmail.com>
2020-11-12 21:52:37 -0700

    Update MapHelper.java

--- src/main/java/me/twoeggs/maphelper/MapHelper.java
+++ src/main/java/me/twoeggs/maphelper/MapHelper.java
@@ -18,7 +18,6 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.commons.lang3.ObjectUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
